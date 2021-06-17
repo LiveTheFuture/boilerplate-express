428e38ec3a3c390e5fb715dbb00a2f5a536a8128
LiveTheFuture <ademijutobi@gmail.com>
2021-06-17 09:39:33 +0100

    Added myApp.js file

--- myApp.js
+++ myApp.js
@@ -1,7 +1,14 @@
+const { static } = require('express');
 var express = require('express');
 var app = express();
 
-console.log('Hello World');
+const absolutePath = __dirname + '/views/index.html';
+
+app.get("/", function(req, res){
+    res.sendFile(absolutePath);
+});
+
+app.use('/public', express.static(__dirname + '/public'));
 
 
 
